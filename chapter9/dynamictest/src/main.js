b08ca9fa99f48e787a3655fdf7ad9af98b53c718
Shin seongsu <tlstjdtn321@naver.com>
2020-02-25 22:54:00 +0900

    vue 컴포넌트 심화

--- chapter9/dynamictest/src/main.js
+++ chapter9/dynamictest/src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
-//import App from './App.vue'
-//import App from './AppNamed.vue'
-import App from './AppScoped.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
