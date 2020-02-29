25c1da1edff80eed8cc92d73695ef63083bd7752
Shin seongsu <tlstjdtn321@naver.com>
2020-02-29 23:57:09 +0900

    연락처 vuex

---
+++ chapter11/contact_vuex/src/Contant.js
@@ -0,0 +1,12 @@
+export default {
+    ADD_CONTACT_FORM : "addContactForm",
+    CANCEL_FORM : "cancelForm",
+    EDIT_CONTACT_FORM : "editContactForm",
+    EDIT_PHOTO_FORM : "editPhotoForm",
+    FETCH_CONTACTS : "fetchContacts",
+
+    ADD_CONTACT : "addContact",
+    UPDATE_CONTACT : "updateContact",
+    UPDATE_PHOTO : "updatePhoto",
+    DELETE_CONTACT : "deleteContact",
+}
