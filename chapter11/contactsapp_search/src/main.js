536701e654d6aea733eac5e3f9b0d7efc5591b07
Shin seongsu <tlstjdtn321@naver.com>
2020-02-29 23:44:23 +0900

    vux axios 연습

--- chapter11/contactsapp_search/src/main.js
+++ chapter11/contactsapp_search/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import store from './store';
+import ES6Promise from 'es6-promise';
+
+ES6Promise.polyfill();
 
 Vue.config.productionTip = false
 
